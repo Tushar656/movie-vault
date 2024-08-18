1e5b15fabd53961d47a383da0484d76bcea5284b
Tushar656 <ui20cs69@iiitsurat.ac.in>
2024-08-18 10:12:40 +0530

    Hide API Key

--- src/api/apicalls.js
+++ src/api/apicalls.js
@@ -1,7 +1,5 @@
 import {TMDB_API_KEY} from '@env'
 
-const apikey = '0274797abee92234ea9dd074b7f616ad';
-
 export const baseImagePath = (size, path) => {
     return `https://image.tmdb.org/t/p/${size}/${path}`;
 }
